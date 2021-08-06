ad2426920b6eccf897381271ae1a3f905ee38b67
AllenOtto <kephamoturi@gmail.com>
2021-08-06 19:41:06 +0300

    implemented reset button functionality

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 // Cache the DOM
 const counter_h1 = document.querySelector(".counter");
-const increment_btn = document.querySelector(".btn-inc");
+const increment_a = document.querySelector(".btn-inc");
 const save_btn = document.querySelector(".btn-save");
 const totalTally_h2 = document.querySelector(".totalTally");
+const reset_a = document.querySelector(".btn-reset");
+var savedCounts = new Array();
 
 // Increment the counter on click
-increment_btn.addEventListener('click', () => {
+increment_a.addEventListener('click', () => {
     counter_h1.innerHTML++;
 });
 
 // Save the value of the counter to the tally
 save_btn.addEventListener('click', () => {
     // Cast strings as Integers for calculation
-    count = Number(counter_h1.innerHTML);
-    tally = Number(totalTally_h2.innerHTML);
+    var count = Number(counter_h1.innerHTML);
+    var tally = Number(totalTally_h2.innerHTML);
 
     // Increment 'tally' with 'count' 
     tally += count;
@@ -23,4 +25,17 @@ save_btn.addEventListener('click', () => {
 
     // Display Current Total Tally
     totalTally_h2.innerHTML = tally;
+});
+
+// Reset Tally Display
+reset_a.addEventListener('click', () => {
+    var tally = Number(totalTally_h2.innerHTML);
+    // Pushes tally to storage
+    savedCounts.push(tally);
+    // Resets total tally
+    totalTally_h2.innerHTML = 0;
+    // Retrieves last item entered in list
+    console.log(savedCounts.slice(-1)[0]);
+    // Shows number of additions to list so far
+    console.log(savedCounts.length);
 });
