78846ff3f16bcedf48fea17582ea541aedd4b2a7
AllenOtto <kephamoturi@gmail.com>
2021-08-06 21:41:37 +0300

    created summary report option and updated updated app.js

--- app.js
+++ app.js
@@ -29,13 +29,19 @@ save_btn.addEventListener('click', () => {
 
 // Reset Tally Display
 reset_a.addEventListener('click', () => {
-    var tally = Number(totalTally_h2.innerHTML);
-    // Pushes tally to storage
-    savedCounts.push(tally);
-    // Resets total tally
-    totalTally_h2.innerHTML = 0;
-    // Retrieves last item entered in list
-    console.log(savedCounts.slice(-1)[0]);
-    // Shows number of additions to list so far
-    console.log(savedCounts.length);
+    if (Number(totalTally_h2.innerHTML > 0)) {
+        var tally = Number(totalTally_h2.innerHTML);
+        // Pushes tally to storage
+        savedCounts.push(tally);
+        // Resets total tally
+        totalTally_h2.innerHTML = 0;
+        // Retrieves last item entered in list
+        console.log(savedCounts.slice(-1)[0]);
+        // Shows number of additions to list so far
+        console.log(savedCounts.length);
+    }else{
+        console.log("No vehicles observed");
+        savedCounts.push(0);
+        console.log(savedCounts.slice(-1)[0]);
+    }
 });
