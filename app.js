21f34ad16832d2d57f8f67e71bee9c5e0c90d36b
AllenOtto <kephamoturi@gmail.com>
2021-08-24 14:35:09 +0300

    app.js updated

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const totalTally_h2 = document.querySelector(".totalTally");
 const reset_a = document.querySelector(".btn-reset");
 const message_p = document.querySelector(".message p");
 var savedCounts = new Array();
-var cumulativeTotal = 0;
 
 // Increment the counter on click
 increment_a.addEventListener("click", () => {
@@ -15,40 +14,31 @@ increment_a.addEventListener("click", () => {
 
 // Save the value of the counter to the tally
 save_btn.addEventListener("click", () => {
-  // Cast strings as Integers for calculation and push 'count' into savedCounts array
+  // Cast strings as Integers for calculation
   var count = Number(counter_h1.innerHTML);
-  savedCounts.push(count);
   var tally = Number(totalTally_h2.innerHTML);
-  // Get tally of all elements in array
+  // Increment 'tally' with 'count'
   tally += count;
+  // Pushes tally to storage
+  savedCounts.push(tally);
   // Reset counter
   counter_h1.innerHTML = 0;
   // Display Current Total Tally
   totalTally_h2.innerHTML = tally;
-  if (savedCounts.length > 0) {
-    cumulativeTotal += count;
-    // Retrieves last item entered in list
-    console.log("Total Cumulative Count: " + cumulativeTotal);
-    // Shows number of additions to storage so far
-    console.log("Save Events So Far: " + savedCounts.length);
-    message_p.innerHTML = "^ " + count;
-    console.log(message_p.classList);
-  }
+  // Retrieves last item entered in list
+  console.log("Last Item in storage: " + savedCounts.slice(-1)[0]);
+  // Shows number of additions to list so far
+  console.log("Number of elements in storage: " + savedCounts.length);
 });
 
 // Reset Tally Display
 reset_a.addEventListener("click", () => {
-  function resetData() {
-    if (Number(totalTally_h2.innerHTML > 0)) {
-      // Resets current total tally
-      totalTally_h2.innerHTML = 0;
-      message_p.classList.toggle("message_visible");
-      message_p.innerHTML = "Data Reset";
-    } else {
-      message_p.innerHTML = "No Current Data";
-    }
+  if (Number(totalTally_h2.innerHTML > 0)) {
+    // Resets total tally
+    totalTally_h2.innerHTML = 0;
+    // Empty the savedCounts data storage array
+    savedCounts.length = 0;
+  } else {
+    console.log("No data");
   }
-
-  var timeId = setInterval(resetData(), 1000);
-  clearInterval(timeId);
 });
